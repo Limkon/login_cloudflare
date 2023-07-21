7f1e2086cf8f1371fe0fad8f92ea0fb0a6f47829
Limkon <130622161+Limkon@users.noreply.github.com>
2023-07-21 11:12:39 +0800

    Add files via upload

--- login.js
+++ login.js
@@ -1,66 +1,60 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 
-const MAX_RETRIES = 3;
-
 (async () => {
-  // 读取 accounts.json 文件中的 JSON 字符串
-  const accountsJson = fs.readFileSync('accounts.json', 'utf-8');
-  const accounts = JSON.parse(accountsJson);
+  const browser = await puppeteer.launch({ headless: false });
 
-  for (const account of accounts) {
-    const { username, password } = account;
+  try {
+    // 读取 accounts.json 文件中的 JSON 字符串
+    const accountsJson = fs.readFileSync('accounts.json', 'utf-8');
+    const accounts = JSON.parse(accountsJson);
 
-    let isLoggedIn = false;
-    let retries = 0;
+    for (const account of accounts) {
+      const { username, password } = account;
 
-    while (!isLoggedIn && retries < MAX_RETRIES) {
-      const browser = await puppeteer.launch({ headless: true });
       const page = await browser.newPage();
 
       try {
-        await page.goto('https://dash.cloudflare.com/login', { waitUntil: 'networkidle0', timeout: 0 });
-
-        // Wait for the login form to be visible
-        await page.waitForSelector('[data-testid="login-form"]', { visible: true, timeout: 30000 });
-
-        // 输入实际的用户名和密码
-        await page.type('#email', username);
-        await page.type('#password', password);
-
-        // Add a delay before submitting the login form (adjust the delay time as needed)
-        await page.waitForTimeout(2000);
-
+        await page.goto('http://wwww.cq17.com:12345/index/Index/Userlogins.html');
+      
+        // 等待页面加载完成
+        await page.waitForSelector('#content_name');
+        await page.waitForSelector('#content_password');
+        await page.waitForSelector('.content_button button');
+      
+        // 输入用户名和密码
+        await page.type('#content_name', username);
+        await page.type('#content_password', password);
+      
         // 提交登录表单
-        await page.click('[name="login-submit-button"]');
-
-        // Wait for successful login (or CAPTCHA/2FA prompt)
-        await page.waitForFunction(() => {
-          const logoutButton = document.querySelector('button[data-testid="logout-button"]');
-          const captchaForm = document.querySelector('form[data-testid="captcha-form"]');
-          const twofaForm = document.querySelector('form[data-testid="login-two-factor-form"]');
-          return logoutButton !== null || captchaForm !== null || twofaForm !== null;
-        }, { timeout: 60000 });
-
-        // Check if login was successful
-        isLoggedIn = await page.evaluate(() => {
-          const logoutButton = document.querySelector('button[data-testid="logout-button"]');
-          return logoutButton !== null;
-        });
-
-        if (isLoggedIn) {
-          console.log(`账号 ${username} 登录成功！`);
-        } else {
-          console.error(`账号 ${username} 登录失败，重试中...`);
-          retries++;
-        }
+        await page.click('.content_button button');
+      
+        // 等待登录成功
+        await page.waitForNavigation();
+      
+        // 转到用户中心页面
+        await page.goto('http://wwww.cq17.com:12345/index/User/index.html');
+      
+        // 等待页面加载完成
+        await page.waitForSelector('.signinqd');
+      
+        // 点击红包领取
+        await page.click('.signinqd');
+      
+        // 在领取红包后执行其他操作...
+      
+        // 点击安全退出
+        await page.click('.quit a');
+      
+        // 等待安全退出完成
+        await page.waitForNavigation();
+      
+        console.log(`账号 ${username} 登录成功！`);
       } catch (error) {
         console.error(`账号 ${username} 登录时出现错误: ${error}`);
-        retries++;
       } finally {
-        // 关闭页面和浏览器
+        // 关闭页面
         await page.close();
-        await browser.close();
 
         // 用户之间添加随机延时
         const delay = Math.floor(Math.random() * 5000) + 1000; // 随机延时1秒到6秒之间
@@ -68,12 +62,13 @@ const MAX_RETRIES = 3;
       }
     }
 
-    if (!isLoggedIn) {
-      console.error(`账号 ${username} 登录失败，重试次数超过最大限制。`);
-    }
+    console.log('所有账号登录成功！');
+  } catch (error) {
+    console.error(`登录时出现错误: ${error}`);
+  } finally {
+    // 关闭浏览器
+    await browser.close();
   }
-
-  console.log('所有账号登录完成！');
 })();
 
 // 自定义延时函数
