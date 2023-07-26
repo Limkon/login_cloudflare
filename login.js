8adc7e8408e6ea17141b1eee6a52292a3ad3c386
Limkon <130622161+Limkon@users.noreply.github.com>
2023-07-26 16:54:10 +0800

    Update login.js

--- login.js
+++ login.js
@@ -1,18 +1,20 @@
-
 const fs = require('fs');
 const puppeteer = require('puppeteer');
+
+// 自定义延时函数
+function delayTime(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
 
-  // 自定义延时函数
-  function delayTime(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+  // 使用缓存存储账号信息
+  let cachedAccounts;
 
   try {
-    // 读取 accounts.json 文件中的 JSON 字符串
-    const accountsJson = fs.readFileSync('accounts.json', 'utf-8');
+    // 读取 accounts.json 文件中的 JSON 字符串，如果缓存存在则使用缓存数据
+    const accountsJson = cachedAccounts || fs.readFileSync('accounts.json', 'utf-8');
     const accounts = JSON.parse(accountsJson);
 
     for (const account of accounts) {
