11c6a6ecc0e84a59ce0538d62a4c0aea63e7fe0a
Limkon <130622161+Limkon@users.noreply.github.com>
2023-11-22 14:14:03 +0800

    Update login.js

--- login.js
+++ login.js
@@ -27,46 +27,9 @@ function delayTime(ms) {
 
   try {
    // 设置登录超时时间
-   page.setRequestTimeout(3000);
+   await page.setNavigationTimeout(3000);
 
-   // 设置重试次数
-   const retries = 3;
-
-   for (let i = 0; i < retries; i++) {
-    // 登录
-    try {
-      // 等待页面加载完成
-      await page.waitForNavigation();
-
-      // 等待 `#content_name` 元素
-      await page.waitForSelector('#content_name');
-
-      // 输入用户名和密码
-      await page.type('#content_name', username);
-      await page.type('#content_password', password);
-
-      // 提交登录表单
-      await page.click('.content_button button');
-
-      // 等待登录成功
-      await page.waitForNavigation();
-
-      // ...
-
-      success = true; // Set success flag to true if login is successful
-      break;
-    } catch (error) {
-      // 登录失败，记录日志
-      console.log(`账号: ${username}`);
-      console.log(`登录结果: 失败`);
-      console.log(`错误信息: ${error}`);
-    }
-   }
-
-   // 登录失败
-   if (i == retries) {
-    console.log(`账号 ${username} 尝试登录 ${retries} 次仍然失败`);
-   }
+   // ...
 
   } catch (error) {
    console.error(`账号 ${username} 登录时出现错误: ${error}`);
